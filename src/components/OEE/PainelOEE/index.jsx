76462c2076c06d420c20b8a7a8910f78fba89c77
Dime Mikio <mikio.dime@gmail.com>
2023-09-12 20:16:22 -0300

    Rev03 - start painel oee

--- src/components/OEE/PainelOEE/index.jsx
+++ src/components/OEE/PainelOEE/index.jsx
@@ -6,20 +6,39 @@ export const PainelOEE = () => {
   return (
     <div className='painel-oee'>
       <div className='painel-oee-container'>
+
         <div className='oee-header-container'>
           <h1>HMC 01</h1>
+          <p>Heller</p>
         </div>
 
         <div className='oee-index-container'>
-          <div>
-            <span>Disponibilidade</span>
+          <div className='oee-index-single'>
+            <span className='oee-index-sub'>Disponibilidade</span>
+            <span>80%</span>
+          </div>
+          <div className='oee-index-single'>
+            <span className='oee-index-sub'>Eficiência</span>
+            <span>80%</span>
+          </div>
+          <div className='oee-index-single'>
+            <span className='oee-index-sub'>Qualidade</span>
+            <span>80%</span>
+          </div>
+          <div className='oee-index-single'>
+            <span className='oee-index-sub'>OEE</span>
             <span>80%</span>
           </div>
         </div>
 
         <div className='oee-infos-container'>
-          <div>Nome</div>
+          <div>Operador</div>
           <div>Turno</div>
+
+          <div>
+            <div><span>Meta Produção</span><span>110</span></div>
+            <div><span>Real Produção</span><span>98</span></div>
+          </div>
         </div>
 
         <div className='oee-parts-container'>
@@ -28,9 +47,16 @@ export const PainelOEE = () => {
             <span>Operação 10</span>
             <span>Tempo Usinagem</span>
             <span>Tempo Troca</span>
-            <span>Tempo Ciclo</span>
+          </div>
+          <div>
+            <span>Peça2</span>
+            <span>Operação 30</span>
+            <span>Tempo Usinagem</span>
+            <span>Tempo Troca</span>
           </div>
         </div>
+        <div>Tempo Ciclo</div>
+
       </div>
     </div>
   )
