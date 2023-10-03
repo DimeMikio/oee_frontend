af8d9cb09342c4f03b242eb4069837a0de915ef0
Dime Mikio <mikio.dime@gmail.com>
2023-10-03 20:45:45 -0300

    teste de tela

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -4,20 +4,20 @@ import '../Global/form/style.css';
 import '../Global/list/style.css';
 
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+/* import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Header } from '../Header';
 import { Menu } from '../Menu';
 import { DashboardOEE } from '../../components/OEE';
 import { Config } from '../../components/Config';
 import { Register } from '../../components/Register';
-
+ */
 
 function Home() {
 
   return (
     <div className="app">
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Menu />
         <div className='header'><Header /></div>
         <div className='main'>
@@ -27,7 +27,7 @@ function Home() {
             <Route path='/register' element={<Register />} />
           </Routes>
         </div>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 }
