b630f31434432f14713480c9e96ebb490486f1ef
Dime Mikio <mikio.dime@gmail.com>
2023-10-03 21:05:19 -0300

    ajuste de janela do app rev2

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -4,14 +4,14 @@ import '../Global/form/style.css';
 import '../Global/list/style.css';
 
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter/* , Routes, Route */ } from 'react-router-dom';
 
-import { Header } from '../Header';
+/* import { Header } from '../Header'; */
 import { Menu } from '../Menu';
-import { DashboardOEE } from '../../components/OEE';
+/* import { DashboardOEE } from '../../components/OEE';
 import { Config } from '../../components/Config';
 import { Register } from '../../components/Register';
-
+ */
 
 function Home() {
 
@@ -19,14 +19,14 @@ function Home() {
     <div className="app">
       <BrowserRouter>
         <Menu />
-        <div className='header'><Header /></div>
-        <div className='main'>
+        {/* <div className='header'><Header /></div> */}
+        {/* <div className='main'>
           <Routes>
             <Route path='/' element={<DashboardOEE />} />
             <Route path='/config' element={<Config />} />
             <Route path='/register' element={<Register />} />
           </Routes>
-        </div>
+        </div> */}
       </BrowserRouter>
     </div>
   );
