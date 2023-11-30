48a9e683cbcd878968c7022501141df7ddeebb45
Dime Mikio <mikio.dime@gmail.com>
2023-11-29 22:26:42 -0300

    start component InputControl ver1

--- src/components/InputControl/index.jsx
+++ src/components/InputControl/index.jsx
@@ -6,6 +6,49 @@ export const InputControl = () => {
       <div className='inputcontrol-bkg'>
         <div className='inputcontrol-painel'>
           
+          <div className='inputcontrol-header'>
+            <p className='header-identy'>hmc01</p>
+            <p className='header-equip'>heller</p>
+          </div>
+
+          <div className='inputcontrol-index'>
+            <div className='index-oee'>
+              <span className='index-oee-circle circle-grn'>
+                <p className='index-value-oee'>87%</p>
+                <p className='index-value-type'>OEE</p>
+              </span>
+            </div>
+
+            <div className='index-others'>
+              <div className='index-sub'>
+                <p className='font-red'>52%</p>
+                <p>Efic</p>
+              </div>
+
+              <div className='index-sub'>
+                <p className='font-ylw'>72%</p>
+                <p>Disp</p>
+              </div>
+
+              <div className='index-sub'>
+                <p className='font-grn'>100%</p>
+                <p>Qual</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='inputcontrol-infos'>
+            <div>
+              <p>Operador</p>
+              <p>dime mikio hosokawa</p>
+            </div>
+
+            <div>
+              <p>Turno</p>
+              <p>2</p>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
